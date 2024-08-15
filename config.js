04239150edf0bab5e253eb0cc54c88efa9479bec
Victor Osinaga <osinagavictor4@gmail.com>
2024-08-15 07:17:00 -0300

    config.js

--- config.js
+++ config.js
@@ -2,45 +2,45 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 const config = {
-  // ______________________________________________________________
-  // PUERTO
+  // PUERTO______________________________________________________________
   PORT: process.env.PORT,
-  // ______________________________________________________________
-  // backend LEGACY STORE ADMIN
+  // backend LEGACY STORE ADMIN______________________________________________________________
   back_legacy_admin_dev : process.env.BACK_LEGACY_ADMIN_DEV,
   back_legacy_admin_prod : process.env.BACK_LEGACY_ADMIN_PROD,
-  // ______________________________________________________________
-  // FRONT PANEL
+  // FRONT PANEL ______________________________________________________________
   front_url_panel_dev: process.env.FRONT_URL_PANEL_DEV,
   front_url_panel_prod: process.env.FRONT_URL_PANEL_PROD,
-  // FRONT STORE
+  // FRONT STORE ______________________________________________________________
   front_url_store_dev: process.env.FRONT_URL_STORE_DEV,
   front_url_store_prod: process.env.FRONT_URL_STORE_PROD,
-  // ORIGIN THIS BACK FOR FETCH
+  // ORIGIN THIS BACK FOR FETCH ______________________________________________________________
   back_origin_url_dev: process.env.BACK_ORIGIN_URL_DEV,
   back_origin_url_prod: process.env.BACK_ORIGIN_URL_PROD,
-  // ______________________________________________________________
-  // DATABASES ADMIN
+  // DATABASES ADMIN ______________________________________________________________
   dev_url_database_admin: process.env.DEV_URL_DATABASE_ADMIN,
   prod_url_database_admin: process.env.PROD_URL_DATABASE_ADMIN,
-  // DATABASES CLIENTS
+  // DATABASES CLIENTS ______________________________________________________________
   dev_url: process.env.DEV_URL,
   prod_url1: process.env.PROD_URL1,
   prod_url2: process.env.PROD_URL2,
-  // ______________________________________________________________
+  // MERCADO PAGO ______________________________________________________________
   access_token_mp: process.env.ACCESS_TOKEN_MP,
   success_url_mp: process.env.SUCCESS_URL_MP,
   failure_url_mp: process.env.FAILURE_URL_MP,
   notification_url_mp: process.env.NOTIFICATION_URL_MP,
+
+  // JWT ______________________________________________________________
   private_key_jwt: process.env.PRIVATE_KEY_JWT,
+  // ENV ______________________________________________________________
   env: process.argv[2],
+
   emailAdmin: process.env.EMAIL_ADMIN,
   google : {
     user: process.env.EMAIL_ADMIN,
     pass: process.env.GMAILPASSWORD
   },
 
-  // FIREBASE CONFIG
+  // FIREBASE CONFIG ______________________________________________________________
   firebaseAccountKey: {
     type: process.env.TYPE,
     project_id: process.env.PROJECT_ID,
